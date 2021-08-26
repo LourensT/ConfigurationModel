04f35798a05ac1338e38e255c54d074dc0d535be
LourensT <touwenlourens@gmail.com>
2021-08-26 17:36:56 +0200

    documentation

--- CM.py
+++ CM.py
@@ -7,6 +7,7 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+from DegreeDistributions.DegreeDistributions import *
 
 class CM:
 
@@ -19,13 +20,10 @@ class CM:
     '''
     def __init__(self, degree_sequence: List[int]):
 
-        for d in degree_sequence:
-            assert d > 0, "non-positive degree"
+        self._checkIfValidDegreeSequence(degr_sequence)
 
         # shuffle the degree distribution to get random order
         np.random.shuffle(degree_sequence)
-
-        print(degree_sequence)
 
         self.G = nx.MultiGraph()
         self.n = len(degree_sequence)
@@ -38,16 +36,19 @@ class CM:
             self.G.add_node(i)
             half_edges[i] = degree_sequence[i]
 
-
+        # generate CM by following steps
+        # - select next vertex (v) which still has unfilled half_edges
+        # - pick another random other vertex (u) and connect it
+        #       - if there are none, self-loops are added to v
 
         while len(half_edges) > 0:
-            print(half_edges)
             keys_left = list(half_edges.keys())
             v = keys_left[0]
             keys_left = keys_left[1:]
 
             v_has_half_edges_left = True
             while v_has_half_edges_left:
+                # self loops to deal with remaining
                 if len(keys_left) == 0:
                     self.G.add_edge(v, v)
                     half_edges[v] -= 2
@@ -59,14 +60,33 @@ class CM:
                     half_edges[v] -= 1
                     half_edges[u] -= 1
                     
+                    # check if (u) still has half_edges left, otherwise remove
                     if half_edges[u] == 0:
                         del half_edges[u]
                         keys_left.remove(u)
 
-                if half_edges[v] <= 0:
+                # check if v still has half_edges, otherwise remove
+                if half_edges[v] == 0:
                     del half_edges[v]
                     v_has_half_edges_left = False
+                # this case should not happen if the sum of degr_sequence is even
+                elif half_edges[v] == -1:
+                    raise ValueError("-1 for last degree, meaning the degree sequence did not sum even.")
+
+
+    def _checkIfValidDegreeSequence(self, degree_sequence):
+        assert not(0 in degr_sequence), "degree sequence contains 0"
+        assert (sum(degr_sequence) % 2 == 0), "degree sequence is not even"
+
                     
+    def DegreeDistribrution(self, tail=True):
+        return DegreeDistribution(self.G, tail=tail)
+
+    def RandomFriendDegreeDistribution(self, tail=True):
+        return RandomFriendDegreeDistribution(self.G, tail=tail)
+
+    def SizeBiasedDegreeDistribution(self, tail=True):
+        return SizeBiasedDegreeDistribution(self.G, tail=tail)
 
     def draw(self):
         nx.draw(self.G)
@@ -75,9 +95,11 @@ class CM:
 
 
 if __name__ == "__main__":
-    while True:
-        cm = CM([1, 1, 2, 1, 2, 3, 4, 5, 6, 3, 4, 5, 6])
-        cm.draw()
+    # generate random CM with degr_sequence
+    degr_sequence = np.random.choice([i for i in range(1, 10)], size=50)
+    print("degree sequence with sum of", sum(degr_sequence))
+    cm = CM(degr_sequence)
+    cm.draw()
             
 
 
